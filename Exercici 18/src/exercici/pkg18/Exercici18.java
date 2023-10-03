5cdd3d0cb29d5dd50d3c0c6e37f4673ba0f3cb40
Adrián Walter Cabezas Altozano <adca5503@proven.local>
2023-10-03 11:39:18 +0200

    3/10/2023

--- Exercici 18/src/exercici/pkg18/Exercici18.java
+++ Exercici 18/src/exercici/pkg18/Exercici18.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author adca5503
+ * @author adric
  */
 public class Exercici18 {
 
@@ -16,17 +16,28 @@ public class Exercici18 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         int hora;
-        int t_normal = 20;
-        int horas_normal=35;
-        
-        
-        Scanner sc = new Scanner(System.in);
-        
-      System.out.println("Cuantas horas trabajas a las semana?");
+        double T_NORMAL = 20;
+        int HORAS_NORMAL = 35;
+        double salario;
+        int h_extra;
+        double salario_extra;
+        double salario_normal;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Pon las horas que trabajas semanalmente:");
         hora = sc.nextInt();
         
-        
+        if (hora > HORAS_NORMAL){
+            h_extra = hora - HORAS_NORMAL;
+            salario_extra = h_extra * T_NORMAL * 1.5;
+            salario_normal = HORAS_NORMAL * T_NORMAL;
+            salario = salario_extra + salario_normal;
+        }
+        else{
+            salario = hora * T_NORMAL;
+        }
+         System.out.println("Tu salario es... " + salario);
     }
     
 }
