5e6a48719ca8353a563abb6b919bf268b97f5fbc
Adrián Walter Cabezas Altozano <adca5503@proven.local>
2023-10-31 11:24:00 +0100

    31/10/23

---
+++ UF2-NF1-ProgramacióModular/Exercici19PM/src/exercici19pm/Exercici19PM.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercici19pm;
+
+/**
+ *
+ * @author adca5503
+ */
+public class Exercici19PM {
+
+    /**
+     * @param args the command line arguments
+     * Realiza un programa que permita comprobar si una terna de valores enteros (3 valores) se ajusta
+     * a la ecuación de Pitágoras: x ² + y ² = z ². El programa solicita al usuario los valores x, y, z. Se
+     * deberá crear una función a la que se le pase x, y, z y devuelva si son iguales o no.
+     * Por ejemplo: 3 ² + 4 ² = 5 ².
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
